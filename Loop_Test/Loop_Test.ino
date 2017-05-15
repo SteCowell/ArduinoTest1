f8bc7c2479d13f8e2334424c9ad948b2ca64646f
SteCowell <stecowell@gmail.com>
2017-05-15 19:53:39 +0100

    Added one comment
    OK, I've sdded a new comment for testing

---
+++ Loop_Test/Loop_Test.ino
@@ -0,0 +1,28 @@
+/* Understanding Loops 
+   Basic code 
+   OK, I've sdded a new comment for testing */
+
+void setup() {
+  Serial.begin (9600);
+}
+
+void loop(void) {
+  char s[2] = " ";
+  uint8_t x, y;
+  for( y = 0; y < 6; y++ ) {
+    for( x = 0; x < 16; x++ ) {
+      s[0] = y*16 + x + 32;
+      Serial.println (x);
+        delay(500);
+      Serial.println (y);
+        delay(500);
+      Serial.println (s);
+        delay(500);
+      Serial.println ("--------------------" + y );
+    }
+  }
+  delay(3000);
+}
+
+
+
